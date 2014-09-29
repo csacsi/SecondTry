43d86f0d2d35dc60fffb5bd367ccd04ec0210dff
Csaba Toth <csacsi@distinction.hu>
2014-09-29 11:29:10 +0200

    added second try

---
+++ thirdTry/thirdtry/SharedClasses/UsefullClassSecond.m
@@ -0,0 +1,17 @@
+//
+//  UsefullClass.m
+//  try
+//
+//  Created by Toth Csaba on 09/09/14.
+//  Copyright (c) 2014 Distinction. All rights reserved.
+//
+
+#import "UsefullClassSecond.h"
+
+@implementation UsefullClassSecond
+
++(NSString*)getUsefullString;
+{
+    return @"Usefull second string v1";
+}
+@end
